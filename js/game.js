47d4c5ccb64cd725ff715b36bf194ba53afcdb35
Caue Queiroz <cauenqueiroz@gmail.com>
2016-11-10 16:33:34 -0200

    Finish play game system

--- js/game.js
+++ js/game.js
@@ -1,6 +1,12 @@
 var Game = {
-    start: false,
+    bank: 100,
+
+    reward: 0,
+
     allowUser: false,
+
+    start: false,
+    
     betValue: 0,
 
     init: function() {
@@ -8,15 +14,29 @@ var Game = {
     },
 
     play: function(event) {
-        if ( Game.start ) return;
+        Game.betValue = event.target.getAttribute('data-value');
+
+        if ( Game.start || !Game.canPlay() ) return;        
+
+        event.target.classList.add('selected');
 
         Game.start = true;
-        Game.betValue = event.target.getAttribute('data-value');
-        Game.turnCard();  
+        Game.bank -= Game.betValue;
+        Game.updateHUD();
+        Game.turnCard();
+    },
+
+    canPlay: function() {
+        return Game.bank >= Game.betValue ? true : false;
+    },
+
+    updateHUD: function() {
+        document.querySelector('.bank').innerHTML = '$ ' + Game.bank;
+        document.querySelector('.reward').innerHTML = '$ ' + Game.reward;
     },
 
     turnCard: function() {
-        
+        console.log('lets play');
     }
 }
 Game.init();
