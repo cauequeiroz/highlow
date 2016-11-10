a95b02aed776a993c409b1905651329073827144
Caue Queiroz <cauenqueiroz@gmail.com>
2016-11-10 15:52:23 -0200

    Add event to play buttons

--- js/game.js
+++ js/game.js
@@ -1,1 +1,22 @@
-alert('oi');
+var Game = {
+    start: false,
+    allowUser: false,
+    betValue: 0,
+
+    init: function() {
+        document.querySelector('.start-buttons').addEventListener('click', Game.play, false);
+    },
+
+    play: function(event) {
+        if ( Game.start ) return;
+
+        Game.start = true;
+        Game.betValue = event.target.getAttribute('data-value');
+        Game.turnCard();  
+    },
+
+    turnCard: function() {
+        
+    }
+}
+Game.init();
